840789b75f5bb3d1d3bc6db8badff128e913be5f
unknown <ayushsalvi19@gmail.com>
2022-12-21 01:46:11 +0530

    Challenge-1

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,25 @@
+
+//My solution : created an array and handled them separately -> checking the char code , can do the same with map
+
+// let keys=document.getElementsByClassName('key');
+// let sounds=document.getElementsByTagName('audio');
+// // console.log(sounds);
+
+// document.addEventListener("keypress",function(e){
+//     let code=e.charCode;
+//     console.log(code);
+//     for(let i=0;i<keys.length;i++)
+//     {
+//         let currCode=Number(keys[i].getAttribute("data-key"));
+//         if(code===currCode)
+//         {
+//             keys[i].classList.add('playing');
+//             sounds[i].currentTime=0;
+//             sounds[i].play();
+//             setTimeout(()=>{
+//                 keys[i].classList.remove('playing');
+//             },70);
+//         }
+//     }
+// });
+
